480a1230e75979dcc2bb437f8d233254463b9602
shiblyshrof <shibly_shrof@yahoo.com>
2014-10-16 02:52:37 +0600

    ShowGridView

---
+++ XyzUniversityAPP/StudentEntryUI.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace XyzUniversityAPP
+{
+    partial class StudentEntryUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.showButton = new System.Windows.Forms.Button();
+            this.studentGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.studentGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(185, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(185, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(185, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Address";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(279, 58);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(208, 20);
+            this.nameTextBox.TabIndex = 0;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(279, 102);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(208, 20);
+            this.emailTextBox.TabIndex = 1;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(279, 145);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(208, 20);
+            this.addressTextBox.TabIndex = 2;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(358, 189);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 3;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(472, 189);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 4;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // studentGridView
+            // 
+            this.studentGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.studentGridView.Location = new System.Drawing.Point(123, 243);
+            this.studentGridView.Name = "studentGridView";
+            this.studentGridView.Size = new System.Drawing.Size(424, 150);
+            this.studentGridView.TabIndex = 5;
+            // 
+            // StudentEntryUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 405);
+            this.Controls.Add(this.studentGridView);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "StudentEntryUI";
+            this.Text = "StudentEntryUI";
+            ((System.ComponentModel.ISupportInitialize)(this.studentGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.DataGridView studentGridView;
+    }
+}
+
---
+++ XyzUniversityAPP/StudentEntryUI.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XyzUniversityAPP
+{
+    public partial class StudentEntryUI : Form
+    {
+        public StudentEntryUI()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+           Student aStudent=new Student();
+            aStudent.Name = nameTextBox.Text;
+            aStudent.Email = emailTextBox.Text;
+            aStudent.Address = addressTextBox.Text;
+
+
+            string conn = @"server=SHIBLY-PC; database=XyzUniversity; integrated security=true";
+
+            SqlConnection Connection = new SqlConnection();
+
+            Connection.ConnectionString = conn;
+            Connection.Open();
+
+            string query = string.Format("INSERT INTO t_Student VALUES ('{0}','{1}','{2}')", aStudent.Name, aStudent.Email, aStudent.Address);
+
+            SqlCommand command = new SqlCommand(query, Connection);
+            int affectedrow = command.ExecuteNonQuery();
+            if (affectedrow > 0)
+
+                MessageBox.Show("insert success");
+
+            else
+
+                MessageBox.Show("problem");
+
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+
+            string conn = @"server=SHIBLY-PC; database=XyzUniversity; integrated security=true";
+
+            SqlConnection Connection = new SqlConnection();
+
+            Connection.ConnectionString = conn;
+            Connection.Open();
+
+            string query = string.Format("SELECT * FROM t_student");
+
+            SqlCommand command = new SqlCommand(query, Connection);
+
+            SqlDataReader aReader = command.ExecuteReader(); 
+
+            List<Student> students=new List<Student>();
+            if(aReader.HasRows)
+            {
+                while (aReader.Read())
+                {
+                    Student aStudent=new Student();
+                    aStudent.StudentId = (int) aReader[0];
+                    aStudent.Name = (string) aReader[1];
+                    aStudent.Email = aReader[2].ToString();
+                    aStudent.Address = aReader[3].ToString();
+                    students.Add(aStudent);
+
+                }
+                
+            }
+            Connection.Close();
+            studentGridView.DataSource = students;
+        }
+    }
+}
